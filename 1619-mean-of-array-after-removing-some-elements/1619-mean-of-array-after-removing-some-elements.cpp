49429985818329a5ef423b28ae93c3cecd5e2ef1
Vansh-Portfolio-Alt <vcperplex461@gmail.com>
2026-07-08 22:32:06 +0530

    Time: 0 ms (100%), Space: 13.1 MB (54.87%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.cpp
@@ -5,7 +5,7 @@ public:
         int k=arr.size()*0.05;
         for(int i=k;i<arr.size()-k;i++){
             sum+=arr[i];
-        }cout<<sum<<' '<<k<<' '<<arr.size();
+        }
         return sum*1.0/(arr.size()-2*k);
     }
 };
